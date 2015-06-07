e48a0150a23c9339751fe099dc188714d54aaaad
dgiac85 <dgiac85@gmail.com>
2015-06-07 13:32:27 +0200

    commento di prova in dgiac85
    Inserito commento di prova

--- app/src/main/java/com/example/giacomo/myandroidugho/MainActivity.java
+++ app/src/main/java/com/example/giacomo/myandroidugho/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //commento di prova
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
